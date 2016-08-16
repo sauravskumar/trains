f8c4a331a2f66ade91dac0b7ec3077ebfab6251c
Saurav Kumar <sauravskumar99@gmail.com>
2016-08-17 00:55:03 +0530

    add react-router 404

--- api/const.js
+++ api/const.js
@@ -3,11 +3,9 @@
  */
 const neo4j = require('neo4j-driver').v1;
 
-const mongo_url = process.env.docker ?
-  'mongodb://trains_mongo:27017/trains' : 'mongodb://172.18.0.2:27017/trains';
+const mongo_url = 'mongodb://trains_mongo:27017/trains';
 
-const neo4j_url = process.env.docker ?
-  'bolt://trains_neo4j' : 'bolt://localhost';
+const neo4j_url = 'bolt://trains_neo4j';
 const neo_driver = neo4j.driver(neo4j_url, neo4j.auth.basic("neo4j", "nike"));
 
 module.exports = {
